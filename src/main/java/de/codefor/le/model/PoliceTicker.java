2b9a24b4c85b037c084528f51ed7681562e25ed7
Sebastian Peters <Sebastian.Peters@gmail.com>
2021-05-08 23:59:16 +0200

    Migrate ES json mapping to annotations

--- src/main/java/de/codefor/le/model/PoliceTicker.java
+++ src/main/java/de/codefor/le/model/PoliceTicker.java
@@ -4,24 +4,38 @@ import java.util.Date;
 
 import org.elasticsearch.common.geo.GeoPoint;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Mapping;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import lombok.Data;
 
 @Data
-@Document(indexName = "policeticker-index", type = "policeticker-type", shards = 1, replicas = 0, refreshInterval = "-1")
-@Mapping(mappingPath = "mapping.json")
+@Document(indexName = "policeticker-index")
 public class PoliceTicker {
 
     @Id
     private String id;
+
+    @Field(type = FieldType.Text, index = false)
     private String url;
+
+    @Field(type = FieldType.Text)
     private String title;
+
+    @Field(type = FieldType.Text)
     private String article;
+
+    @Field(type = FieldType.Text, index = false)
     private String snippet;
+
+    @Field(type = FieldType.Text, index = false)
     private String copyright;
+
+    @Field(type = FieldType.Date, format = DateFormat.basic_date_time)
     private Date datePublished;
+
     private GeoPoint coords;
 
 }
